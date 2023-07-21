41a47047afdf7976a79b5ad7c5a889174cbb1f76
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-07-21 13:10:38 +0300

    config api app and hello world url view setup

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
